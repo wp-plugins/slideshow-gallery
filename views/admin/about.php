32b1171f31c81d022b2cdad136901864d57bd6c9
contrid <contrid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-07 13:17:03 +0000

    Added about page
    git-svn-id: https://plugins.svn.wordpress.org/slideshow-gallery/trunk@961687 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/admin/about.php
+++ views/admin/about.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Newsletters About Dashboard v4.3.6
+ * Slideshow Gallery About Dashboard v1.4.4.3
  */
 
 /** WordPress Administration Bootstrap */
@@ -11,9 +11,9 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 ?>
 
 <div class="wrap newsletters about-wrap">
-	<h1><?php echo sprintf(__( 'Welcome to Newsletters %s', $this -> plugin_name), $this -> version); ?></h1>
+	<h1><?php echo sprintf(__( 'Welcome to Slideshow Gallery %s', $this -> plugin_name), $this -> version); ?></h1>
 	<div class="about-text">
-		<?php echo sprintf(__('Thank you for installing! Newsletters %s is more powerful, reliable and versatile than before. It includes many features and improvements to make email marketing easier and more efficient for you.', $this -> plugin_name), $this -> version); ?>
+		<?php echo sprintf(__('Thank you for installing! Slideshow Gallery %s is more powerful, reliable and versatile than before. It includes many features and improvements.', $this -> plugin_name), $this -> version); ?>
 	</div>
 	<div class="newsletters-badge"><?php echo sprintf(__('Version %s', $this -> plugin_name), $this -> version); ?></div>
 	
@@ -26,16 +26,16 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 				<p><?php _e('This version is 100% compatible with the latest WordPress version. It will fit nicely into your WordPress dashboard and maximizes the WordPress capabilities for speed, functionality and reliability.', $this -> plugin_name); ?></p>
 			</div>
 			<div class="col-2">
-				<img src="<?php echo $this -> url(); ?>/images/about/feature-4.png">
-				<h4><?php _e('WPML integration', $this -> plugin_name); ?></h4>
-				<p><?php _e('This version of the Newsletter plugin is fully integrated with WPML. It now supports internationalization and multilanguage through WPML.', $this -> plugin_name); ?></p>
+				<img src="<?php echo $this -> url(); ?>/images/about/feature-2.jpg">
+				<h4><?php _e('Multilingual', $this -> plugin_name); ?></h4>
+				<p><?php _e('This version of the Slideshow Gallery plugin is fully integrated with (m)qTranslate. It now supports internationalization and multilanguage through (m)qTranslate.', $this -> plugin_name); ?></p>
  <?php /* _e('Easier Editing Tools', $this -> plugin_name); ?></h4>
 				<p><?php _e('Improved visual editing of themes and several other areas throughout the plugin to help you see better what the end-product will look like without having to necessarily go into code.', $this -> plugin_name); */?></p>
 			</div>
 			<div class="col-3 last-feature">
 				<img src="<?php echo $this -> url(); ?>/images/about/feature-3.jpg">
-				<h4><?php _e('JSON API', $this -> plugin_name); ?></h4>
-				<p><?php _e('The JSON API is a direct bridge and communication channel between any code or application and the Newsletter plugin itself, including it\'s database.', $this -> plugin_name); ?></p>
+				<h4><?php _e('Responsive Slideshow', $this -> plugin_name); ?></h4>
+				<p><?php _e('The new, responsive option is a flexible foundation that adapts your slideshow to mobile devices and the desktop or any other viewing environment. In this way your slideshow can easily be viewed on a desktop or mobile device.', $this -> plugin_name); ?></p>
 			</div>
 		</div>
 	</div>
@@ -44,65 +44,44 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 
 	<div class="feature-section col two-col">
 		<div class="col-1">
-			<img src="<?php echo $this -> url(); ?>/images/about/feature-7.png">			
-			<h4><?php _e('Clicks section in admin', $this -> plugin_name); ?></h4> 
-			<p><?php _e('A new clicks section in admin to view and manage all clicks on links. You can view all links and clicks per individual subscriber and all clicks per link.', $this -> plugin_name); ?>
+			<img src="<?php echo $this -> url(); ?>/images/about/feature-4.jpg">			
+			<h4><?php _e('Compatibility with Thickbox', $this -> plugin_name); ?></h4> 
+			<p><?php _e(' Slideshows in this version is compatibile with Thickbox/Lightbox to show slide images in an overlay.', $this -> plugin_name); ?>
 			<?php /* _e('Spam Score', $this -> plugin_name); ?></h4>
 			<p><?php _e('Prevent your emails from ending up in spam/junk folder unnecessarily. The spam score utility will show on each newsletter you create to give the newsletter a score, taking all it\'s aspects and factors in consideration.', $this -> plugin_name); */?></p>
 		</div>
 		<div class="col-2 last-feature">
-						<img src="<?php echo $this -> url(); ?>/images/about/feature-8.png">
-						<h4><?php _e('Filter Subscribers', $this -> plugin_name); ?></h4>
-						<p><?php _e('You can now filter subscribers per mailinglist, status and registered status. After filtering your subscribers, you can activate or deactivate them, assign them to another mailinglist or delete them.', $this -> plugin_name); ?>
+						<img src="<?php echo $this -> url(); ?>/images/about/feature-5.jpg">
+						<h4><?php _e('More than one slideshow', $this -> plugin_name); ?></h4>
+						<p><?php _e('Create a beautiful page with more than one slideshow. You now have the ability to add unlimited slideshows per page, as many as you want. They will all play along nicely!', $this -> plugin_name); ?>
 			<p><?php /*_e('The plugin can automatically send a "Sorry to see you go..." email to a user when they unsubscribe to both confirm their subscription, express your disappointment that they are leaving and it also includes a resubscribe link to convert.', $this -> plugin_name); */?></p>
 		</div>
 </div>
 
 <hr>
-	
 		<div class="changelog under-the-hood">
-		<h3><?php _e( 'New Extensions', $this -> plugin_name); ?></h3>
-		<div class="feature-section col three-col">
-			<div class="col-1">
-				<h4><?php _e('Digital Access Pass', $this -> plugin_name); ?></h4>
-				<p><?php _e('This extension plugin allows you to capture Newsletter plugin subscribers from your 3rd party Digital Access Pass application/platform. As customers purchase products/memberships through Digital Access Pass, they will be added as subscribers.', $this -> plugin_name); ?></p>
-				<p> <a href="http://tribulant.com/extensions/view/43/digital-access-pass" class="button button button-primary">Digital Access Pass</a></p>
-			</div>
-			<div class="col-2">
-				<h4><?php _e('WooCommerce Subscribers', $this -> plugin_name); ?></h4>
-				<p><?php _e('WooCommerce Subscribers is a free extension plugin to capture email subscribers from your WooCommerce plugin checkout procedure into your Newsletter plugin.', $this -> plugin_name); ?></p>
-				<p> <a href="http://tribulant.com/extensions/view/42/woocommerce-subscribers" class="button button button-primary">WooCommerce Subscribers</a></p>
-			</div>
-			<div class="col-3 last-feature">
-				<h4><?php _e('Google Analytics', $this -> plugin_name); ?></h4>
-				<p><?php _e('Google Analytics helps you analyze visitor traffic and paint a complete picture of your audience and their needs, wherever they are along the path.', $this -> plugin_name); ?></p>
-				<p> <a href="http://tribulant.com/extensions/view/46/google-analytics" class="button button button-primary">Google Analytics</a></p>
-			</div>
-		</div>
-	</div>
-	<div class="changelog under-the-hood">
 		<h3>Under the Hood</h3>
 	
 		<div class="feature-section col three-col">
 		<div>
-		<h4><?php _e('Auto import users', $this -> plugin_name); ?></h4>
-		<p><?php _e('The new auto import user function prevent unsubscribe/bounce emails and users from importing again.', $this -> plugin_name); ?></p>		
+		<h4><?php _e('Auto Slide', $this -> plugin_name); ?></h4>
+		<p><?php _e('Set your slideshow to autoslide, when viewing a slideshow it will autoslide and it won\'t be necessary for the user to manually flip through the images.', $this -> plugin_name); ?></p>		
 		<h4><?php _e('WordPress Object Cache API', $this -> plugin_name); ?></h4>
-		<p><?php _e('peed up the plugin with the WordPress Object Cache API which is now built in to cache queries through the WordPress database object.', $this -> plugin_name); ?></p>
+		<p><?php _e('Speed up the plugin with the WordPress Object Cache API which is now built in to cache queries through the WordPress database object.', $this -> plugin_name); ?></p>
 		</div>
 		<div>
-		<h4><?php _e('Edit themes directly while creating a newsletter', $this -> plugin_name); ?></h4>
-		<p><?php _e('You can now edit a theme directly as you are busy creating a newsletter.', $this -> plugin_name); ?></p>
+		<h4><?php _e('Hide information bar on mobile devices', $this -> plugin_name); ?></h4>
+		<p><?php _e('Hide the information bar on mobile devices with a responsive slideshow to view a full slideshow without any text over it..', $this -> plugin_name); ?></p>
 		
-		<h4><?php _e('Paid subscriptions immediate payment', $this -> plugin_name); ?></h4>
-		<p><?php _e('This function allow paid subscribers to pay immediately for a subscription and not being redirect and ask to confirm on email.', $this -> plugin_name); ?></p>
+		<h4><?php _e('Show latest/feature products from Shopping Cart plugin', $this -> plugin_name); ?></h4>
+		<p><?php _e('Add a slideshow to show your products from the WordPress Shopping Cart plugin. You can choose to show either latest products or feature products images in the slideshow. The images, titles, descriptions, etc. will be automatically pulled from the Shopping Cart plugin, it is fully automated and integrated.', $this -> plugin_name); ?></p>
 		</div>
 		<div class="last-feature">
-		<h4><?php _e('Auto save information while creating a newsletter', $this -> plugin_name); ?></h4>
-		<p><?php _e('Auto save information inserted into a newsletter while busy creating it.', $this -> plugin_name); ?></p>
+		<h4><?php _e('Revamp of Configuration Settings', $this -> plugin_name); ?></h4>
+		<p><?php _e('The Configurations Settings got a Revamp. New Sliders for speed settings, Color picker for color settings, Debugging setting and more.', $this -> plugin_name); ?></p>
 		
-		<h4><?php _e('More Hooks', $this -> plugin_name); ?></h4>
-		<p><?php _e('We\'ve added more action and filter hooks to the core of the plugin which can be used by developers to integrate their own apps or 3rd party plugins as needed. If you need specific hooks, please contact us and we\'ll add it.', $this -> plugin_name); ?></p>
+		<h4><?php _e('Child Theme Folder Support', $this -> plugin_name); ?></h4>
+		<p><?php _e('The best way to make modifications to template files in the Slideshow Gallery plugin is to create a child theme folder for the plugin inside your WordPress theme folder. This version now supports a child theme folder.', $this -> plugin_name); ?></p>
 		</div>
 		
 		</div>
@@ -110,7 +89,7 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 		<hr>
 		
 		<div class="return-to-dashboard">
-		<a href="<?php echo admin_url('admin.php'); ?>?page=newsletters"><?php _e('Go to Newsletters overview', $this -> plugin_name); ?></a>
+		<a href="<?php echo admin_url('admin.php'); ?>?page=slideshow"><?php _e('Go to Slideshow Gallery overview', $this -> plugin_name); ?></a>
 		</div>
 	
 	</div>
